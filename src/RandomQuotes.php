92aa8f096abea0312231d60fab41f3148e9442fa
Lalit Kumar Dudeja <info@lalitdudeja.com>
2020-06-28 01:30:34 +0530

    Composer file added

---
+++ src/RandomQuotes.php
@@ -0,0 +1,26 @@
+<?php
+namespace RandomQuotes;
+
+class RandomQuotes
+{  
+  public function getRandomQuotes()
+  {  
+	//get the list of quotes.
+	$quotes = file_get_contents(__DIR__.'../inc/quotes.json');
+
+	//convert JSON document to PHP array
+	$quotes = json_decode($quotes,true);
+
+	//Get a random index number
+	$index = mt_rand(0, count($quotes));
+
+	//Return the random quote.
+	return $quotes[$index];  
+  }
+
+  public function generate()
+  {
+	return $this->getRandomQuotes();
+  }
+}
+?>
